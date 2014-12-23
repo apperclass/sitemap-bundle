83ed6b0726049e7b7b4b85d4a2978d6bf9641b57
Ruben Barilani <ruben.barilani.dev@gmail.com>
2014-12-23 11:59:01 +0100

    improve coverage

--- src/Tests/Fixtures/Application/TestableSitemapUrlProvider.php
+++ src/Tests/Fixtures/Application/TestableSitemapUrlProvider.php
@@ -10,9 +10,11 @@
 namespace Apperclass\Bundle\SitemapBundle\Tests\Fixtures\Application;
 
 
+use Apperclass\Bundle\SitemapBundle\Sitemap\SitemapImage;
 use Apperclass\Bundle\SitemapBundle\Sitemap\SitemapInterface;
 use Apperclass\Bundle\SitemapBundle\Sitemap\SitemapUrl;
 use Apperclass\Bundle\SitemapBundle\Sitemap\SitemapUrlProviderInterface;
+use Apperclass\Bundle\SitemapBundle\Sitemap\SitemapVideo;
 
 class TestableSitemapUrlProvider implements SitemapUrlProviderInterface{
 
@@ -46,6 +48,10 @@ class TestableSitemapUrlProvider implements SitemapUrlProviderInterface{
         $post->setLoc('/posts/1');
         $post->setLastmod((new \DateTime())->format('Y-m-d'));
         $post->setPriority('0.5');
+        $post->setChangefreq('monthly');
+
+        $post->setImages($this->getRandomSitemapImages());
+        $post->setVideos($this->getRandomSitemapVideos());
 
         $comment = new SitemapUrl();
         $comment->setLoc('/posts/1/comments/2');
@@ -54,6 +60,36 @@ class TestableSitemapUrlProvider implements SitemapUrlProviderInterface{
 
         $sitemap->add($post);
         $sitemap->add($comment);
+    }
+
+    protected function getRandomSitemapImages()
+    {
+        $images = array();
+        for ($i=0; $i < rand(0,5); $i++) {
+            $sitemapImage = new SitemapImage();
+            $sitemapImage->setLoc(sprintf('http://website.com/%s.png', uniqid()));
+
+            $sitemapImage->setGeoLocation('Milan');
+
+            $sitemapImage->setCaption('Caption Test');
+
+            $sitemapImage->setTitle('Title Test');
+
+            $sitemapImage->setLicense('License Test');
+
+            $images[] = $sitemapImage;
+        }
+        return $images;
+    }
+    protected function getRandomSitemapVideos()
+    {
+        $videos = array();
+        for ($i=0; $i < rand(0,5); $i++) {
+            $sitemapVideo = new SitemapVideo();
+            // @TODO add properties
+            $videos[] = $sitemapVideo;
+        }
+        return $videos;
     }
 }
 
